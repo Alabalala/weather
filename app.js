485fb1b7af46c9b383468cb2c10f770fafc71896
Alabalala <allareiksa@gmail.com>
2024-08-20 14:43:21 +0200

    second commit

--- app.js
+++ app.js
@@ -51,10 +51,3 @@ function changeBg(celciusTemp) {
       break;
   }
 }
-
-function fibonacci(number) {
-  if (number < 2) {
-    return number;
-  }
-  return fibonacci(number - 1) + fibonacci(number - 2);
-}
